f800c45a379aee2b7b80e7f8fbcd4d3795840ee9
Tobias Olofsson <tobbe.bia@gmail.com>
2012-04-27 19:00:40 +0200

    worked on the screen device

--- devices/screen/screen.c
+++ devices/screen/screen.c
@@ -1,10 +1,12 @@
 #include "screen.h"
 
-/* Sets the target dcpu16_device_t structure to represent the screen. */
-void screen_device(dcpu16_device_t * dev)
+static void (* screen_changed_callback)(char x, char y, DCPU16_WORD value) = 0;
+static DCPU16_WORD screen_buffer[SCREEN_COLUMNS * SCREEN_ROWS];
+
+void screen_create_device(dcpu16_device_t * dev)
 {
-	dev->ram_start_address = 0;
-	dev->ram_end_address = 0;
+	dev->ram_start_address = SCREEN_RAM_START_ADDRESS;
+	dev->ram_end_address = SCREEN_RAM_END_ADDRESS;
 
 	dev->initialize = screen_initialize;
 	dev->release = screen_release;
@@ -12,24 +14,30 @@ void screen_device(dcpu16_device_t * dev)
 	dev->read = screen_read;
 }
 
+void screen_set_callback(void (* callback)(char x, char y, DCPU16_WORD value))
+{
+	screen_changed_callback = callback;
+}
+
 static void screen_initialize()
 {
-
+	memset(screen_buffer, 0, sizeof(screen_buffer));
 }
 
 static void screen_release()
 {
-
 }
 
 static void screen_write(DCPU16_WORD address, DCPU16_WORD value)
 {
+	screen_buffer[address] = value;
 
+	if(screen_changed_callback)
+		screen_changed_callback(address / SCREEN_COLUMNS, address % SCREEN_COLUMNS, value);
 }
 
 static DCPU16_WORD screen_read(DCPU16_WORD address)
 {
-
+	return screen_buffer[address];
 }
-
 
--- devices/screen/screen.h
+++ devices/screen/screen.h
@@ -3,6 +3,13 @@
 
 #include "dcpu16.h"
 
-void screen_device(dcpu16_device_t * dev);
+#define SCREEN_COLUMNS			// INSERT VALUE HERE
+#define SCREEN_ROWS			// INSERT VALUE HERE
+
+#define SCREEN_RAM_START_ADDRESS 	// INSERT VALUE HERE
+#define SCREEN_RAM_END_ADDRESS 		SCREEN_RAM_START_ADDRESS + SCREEN_COLUMNS * SCREEN_ROWS
+
+void screen_create_device(dcpu16_device_t * dev);
+void screen_set_callback(void (* callback)(char x, char y, DCPU16_WORD value));
 
 #endif
